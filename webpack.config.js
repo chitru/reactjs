76e7a030b8b8a7dafaa35b6cb1f00ec367896416
chitru_shrestha <schitru@gmail.com>
2017-01-18 17:18:41 +0545

    first component made

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,20 @@
-var webpack = require("webpack");
 var path = require("path");
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC_DIR + "/app/index.js", 
+    entry: SRC_DIR + "/app/index.js",
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
-        publicPath: "/app/" 
+        publicPath: "/app/"
     },
-    modules: {
+    module: {
         loaders: [
             {
                 test: /\.js?/,
-                include: SRC_DIR, 
+                include: SRC_DIR,
                 loader: "babel-loader",
                 query: {
                     presets: ["react", "es2015", "stage-2"]
